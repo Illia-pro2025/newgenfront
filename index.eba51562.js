544c68b93fff68264b6bfbbe5219c9b99292533f
Illia-pro2025 <illamyrovec05@gmail.com>
2025-11-15 11:50:01 +0100

    Updates

---
+++ index.eba51562.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelectorAll(".gallery__images__item active"),c=0;e.forEach((function(t,i){t.addEventListener("click",(function(){e[c].classList.remove("active"),c=(i+1)%e.length,e[c].classList.add("active"),console.log("-")}))}))}();
+//# sourceMappingURL=index.eba51562.js.map
